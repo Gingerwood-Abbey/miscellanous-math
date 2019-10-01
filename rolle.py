fd9845d09b34e1ca68d6e7cbf88fe6d8d8629926
rakshithss01 <55758038+rakshithss01@users.noreply.github.com>
2019-10-01 10:06:42 +0530

    Update rolle.py

--- rolle.py
+++ rolle.py
@@ -11,13 +11,16 @@ a = -2
 b = 2
 is_valid = True
 for c in linspace(a,b,20):
+#checking for continuity i.e  lhl = rhl= f(c) here;
     t = y.subs({x:c}).evalf()
     t1 = y.subs({x:c-h}).evalf()
     t2 = y.subs({x:c+h}).evalf()
     w1 = Limit(t1,h,0).doit()
     w2 = Limit(t2,h,0).doit()
+#checking for differntiability i.e LHD = RHD;
     y1 = Limit((t1-y)/h,h,0).doit()
     y2 = Limit((y-t2)/h,h,0).doit()
+#checking if the function is continuous and differentiable;
     if (t != w1 and t != w2 and y1!=y2):
       is_valid= False
       break
@@ -25,8 +28,12 @@ if is_valid:
    print("The Function is Continous in [",a,",",b,"]")
    print("The Function is Differentiable in (",a,",",b,")")
    print("Rolle's Theorem is verified")
+#finding the first derivative 
    y1 = Derivative(y,x).doit()
-   print(simplify(solve(y1)))
+# finding the point where x=0; using solve
+   g=solve(y1)
+   j = simplify(g)
+   print("The point c is: ",j)
 else:
      print("The Function is not Continous in [",a,",",b,"]")
      print("The Function is not Differentiable in (",a,",",b,")")
