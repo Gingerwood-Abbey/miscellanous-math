7157f69a1c564d2d712ab7bfd1ba95bb29a75adc
rakshithss01 <55758038+rakshithss01@users.noreply.github.com>
2019-12-29 01:27:23 +0530

    Update curve_plot.py

--- curve_plot.py
+++ curve_plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # referred from https://mmas.github.io/conics-matplotlib 
+#plotting curves
 import matplotlib.pyplot as plt
 import numpy as np
 a= 4
@@ -14,6 +15,7 @@ def axes():
     plt.ylabel('y')
 def curve():
     #type the function as plt.contour(x,y,function)
+    axes()
     plt.contour(x,y,(x**2/a**2 +y**2/b**2 -1))
     plt.show()
 curve()
